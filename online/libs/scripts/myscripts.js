c89b4990b456f941d66a3111603dbff0c9e14168
Muzahidul Islam <muzahidul190@gmail.com>
2021-11-14 20:05:08 +0600

    Add files via upload

---
+++ online/libs/scripts/myscripts.js
@@ -0,0 +1,27 @@
+//Write your scripts here....
+
+
+
+
+
+
+
+
+
+
+
+// Codes for Tooltip
+var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+// Codes for popover
+var popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+});
